1e907df6625008768c92ba382cf1807918198d53
edvards Torsters-Makauskis <BasistaPlay@gmail.com>
2024-02-05 12:12:06 +0000

    a

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,91 +1,127 @@
-'use strict';
 
-// all initial elements
-const payAmountBtn = document.querySelector('#payAmount');
-const decrementBtn = document.querySelectorAll('#decrement');
-const quantityElem = document.querySelectorAll('#quantity');
-const incrementBtn = document.querySelectorAll('#increment');
-const priceElem = document.querySelectorAll('#price');
-const subtotalElem = document.querySelector('#subtotal');
-const taxElem = document.querySelector('#tax');
-const totalElem = document.querySelector('#total');
+updateTotal();
+function updateTotal() {
+    var subtotalElement = document.getElementById('subtotal');
+    var taxElement = document.getElementById('tax');
+    var totalElement = document.getElementById('total');
+    var totalElements = document.getElementById('total-price');
+    var cuponElement = document.getElementById('shipping');
+    var payAmount = document.getElementById('payAmount');
 
+    var totalElementsValue = parseFloat(totalElements.innerText);
+    var cuponElement = document.querySelector('.Cupon');
 
-// loop: for add event on multiple `increment` & `decrement` button
-for (let i = 0; i < incrementBtn.length; i++) {
+    if (cuponElement) {
+        var shippingElement = cuponElement.querySelector('#shipping');
+    
+        if (shippingElement) {
+            var cuponElementValue = parseFloat(shippingElement.innerText);
+        } 
 
-  incrementBtn[i].addEventListener('click', function () {
-
-    // collect the value of `quantity` textContent,
-    // based on clicked `increment` button sibling.
-    let increment = Number(this.previousElementSibling.textContent);
-
-    // plus `increment` variable value by 1
-    increment++;
-
-    // show the `increment` variable value on `quantity` element
-    // based on clicked `increment` button sibling.
-    this.previousElementSibling.textContent = increment;
-
-    totalCalc();
-
-  });
-
-
-  decrementBtn[i].addEventListener('click', function () {
-
-    // collect the value of `quantity` textContent,
-    // based on clicked `decrement` button sibling.
-    let decrement = Number(this.nextElementSibling.textContent);
-
-    // minus `decrement` variable value by 1 based on condition
-    decrement <= 1 ? 1 : decrement--;
-
-    // show the `decrement` variable value on `quantity` element
-    // based on clicked `decrement` button sibling.
-    this.nextElementSibling.textContent = decrement;
-
-    totalCalc();
-
-  });
-
-}
-
-
-
-// function: for calculating total amount of product price
-const totalCalc = function () {
-
-  // declare all initial variable
-  const tax = 0.05;
-  let subtotal = 0;
-  let totalTax = 0;
-  let total = 0;
-
-  // loop: for calculating `subtotal` value from every single product
-  for (let i = 0; i < quantityElem.length; i++) {
-
-    subtotal += Number(quantityElem[i].textContent) * Number(priceElem[i].textContent);
-
+    }
+    var taxRate = 0.21;
+    var taxValue = totalElementsValue * taxRate;
+  
+    taxElement.innerText = taxValue.toFixed(2);
+  
+    var subtotalValue = totalElementsValue - taxValue + cuponElementValue;
+  
+    subtotalElement.innerText = subtotalValue.toFixed(2);
+  
+    var totalValue = subtotalValue + taxValue;
+  
+    totalElement.innerText = totalValue.toFixed(2);
+    payAmount.innerText = totalValue.toFixed(2);
   }
 
-  // show the `subtotal` variable value on `subtotalElem` element
-  subtotalElem.textContent = subtotal.toFixed(2);
 
-  // calculating the `totalTax`
-  totalTax = subtotal * tax;
 
-  // show the `totalTax` on `taxElem` element
-  taxElem.textContent = totalTax.toFixed(2);
+  document.addEventListener('DOMContentLoaded', function () {
+    const validateButton = document.getElementById('validate-button');
+    const discountTokenInput = document.getElementById('discount-token');
+    const validationResult = document.getElementById('validation-result');
+    const subtotalElement = document.getElementById('subtotal');
+    const cuponElement = document.querySelector('.Cupon');
 
-  // calcualting the `total`
-  total = subtotal + totalTax;
+    validateButton.addEventListener('click', function () {
+        const discountCode = discountTokenInput.value;
 
-  // show the `total` variable value on `totalElem` & `payAmountBtn` element
-  totalElem.textContent = total.toFixed(2);
-  payAmountBtn.textContent = total.toFixed(2);
+        fetch('/check_discount_code/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-CSRFToken': getCookie('csrftoken')
+            },
+            body: `discount-token=${encodeURIComponent(discountCode)}`
+        })
+        .then(response => response.json())
+        .then(data => {
+            handleDiscountCodeResponse(data);
+        })
+        .catch(error => {
+            console.error('Kļūda veicot pieprasījumu:', error);
+        });
+    });
 
-}
+    function handleDiscountCodeResponse(data) {
+        if (data.valid) {
+            if (data.quantity !== null && data.quantity > 0) {
+                data.quantity -= 1;
+                updateQuantityOnServer(data.discountCode, data.quantity);
+            }
+    
+            if (data.isExpired) {
+                validationResult.innerHTML = 'Atlaides kods nav derīgs, jo termiņš ir beidzies.';
+            } else if (data.isActivated) {
+                validationResult.innerHTML = 'Atlaides kods ir derīgs, bet jau ir aktivizēts.';
+            } else {
+                const currencySymbol = data.type === 'percentage' ? '%' : '€';
+                validationResult.innerHTML = `Atlaides kods ir derīgs! Vērtība: ${data.value}${currencySymbol}`;
+                
+                const discountAmount = calculateDiscountAmount(data.type, data.value, parseFloat(subtotalElement.textContent));
+                displayDiscountInformation(discountAmount);
+                updateTotal();
+            }
+        } else {
+            validationResult.innerHTML = data.message || 'Ievadītais kods nav derīgs.';
+            hideDiscountInformation();
+        }
+    }
+
+    function updateQuantityOnServer(discountCode, newQuantity) {
+        fetch('/update_quantity/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken')
+            },
+            body: JSON.stringify({ discountCode, quantity: newQuantity })
+        })
+        .catch(error => {
+            console.error('Kļūda veicot pieprasījumu:', error);
+        });
+    }
+
+    function calculateDiscountAmount(type, value, subtotal) {
+        return type === 'percentage' ? (parseFloat(value) / 100) * subtotal : parseFloat(value);
+    }
+
+    function displayDiscountInformation(discountAmount) {
+        cuponElement.style.display = 'flex';
+        cuponElement.querySelector('#shipping').textContent = -discountAmount.toFixed(2);
+    }
+
+    function hideDiscountInformation() {
+        cuponElement.style.display = 'none';
+    }
+
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        return parts.length === 2 ? parts.pop().split(';').shift() : null;
+    }
+});
+
 
 function limitDigits(input, maxLength) {
     if (input.value.length > maxLength) {
@@ -95,10 +131,9 @@ function limitDigits(input, maxLength) {
 
 function formatCardNumber(input) {
     let cardNumber = input.value.replace(/\D/g, '');
-    cardNumber = cardNumber.replace(/(\d{4})(?=\d)/g, '$1 ');
+    cardNumber = cardNumber.replace(/(\d{4})(?=\d)/g, '€1 ');
     input.value = cardNumber;
 
-    // Noskaidro, kura karte ir ievadīta
     checkCardType(cardNumber);
 }
 
@@ -107,18 +142,14 @@ function checkCardType(cardNumber) {
     const cardTypeElement = document.getElementById('card-type');
     const cardImageElement = document.getElementById('card-image');
 
-    // Definē kredītkartes tipus un to numuru diapazonus
     const cardTypes = [
         { type: 'Visa', pattern: /^4/ },
         { type: 'MasterCard', pattern: /^5[1-5]/ },
         { type: 'amex', pattern: /^3[47]/ },
-        // Papildiniet ar citiem kredītkartes tipiem un to numuru diapazoniem
     ];
 
-    // Atrod atbilstošo kredītkartes tipu
     const matchingCardType = cardTypes.find(card => card.pattern.test(cardNumber));
 
-    // Parāda rezultātu lietotājam
     if (matchingCardType) {
         cardTypeElement.innerHTML = `<i class="fa-brands fa-cc-${matchingCardType.type.toLowerCase()}"></i>`;
         showCardImage(matchingCardType.type);
@@ -131,13 +162,53 @@ function checkCardType(cardNumber) {
 function showCardImage(cardType) {
     const cardImageElement = document.getElementById('card-image');
 
-    // Pievieno Font Awesome ikonu attēlojumu kartes tipam
     cardImageElement.innerHTML = `<i class="fa-brands fa-cc-${cardType.toLowerCase()}"></i>`;
 }
 
 function hideCardImage() {
     const cardImageElement = document.getElementById('card-image');
     
-    // Paslēpj ikonu, ja kartes tips nav atpazīts
     cardImageElement.innerHTML = '';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const decrementLinks = document.querySelectorAll('.decrement-link');
+
+    decrementLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const productId = this.dataset.productId;
+            decrementItem(productId);
+        });
+    });
+
+    function decrementItem(productId) {
+        fetch(`/decrement_item/${productId}/`, {
+            method: 'GET', // vai 'POST', atkarībā no jūsu servera konfigurācijas
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken') // Ja izmantojat CSRF aizsardzību
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Atjaunināt lapas saturu, piemēram, span ar id="quantity"
+                const quantityElement = document.getElementById('quantity');
+                quantityElement.textContent = data.new_quantity;
+            } else {
+                console.error('Neizdevās samazināt!');
+            }
+        })
+        .catch(error => {
+            console.error('Kļūda veicot pieprasījumu:', error);
+        });
+    }
+
+    // Palīgfunkcija CSRF žetona iegūšanai
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        return parts.length === 2 ? parts.pop().split(';').shift() : null;
+    }
+});
